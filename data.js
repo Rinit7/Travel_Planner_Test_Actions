4e1830f2e78ed98e9e1c50a1a7571c537f13f03a
Rinit7 <swanitrivankar@gmail.com>
2026-01-14 00:55:30 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const TRAVEL_DATA = [
+  {
+    name: "Goa, India",
+    places: ["Baga Beach", "Fort Aguada", "Chapora Fort", "Anjuna Beach"],
+    food: ["Seafood Thali", "Vindaloo", "Bebinca"],
+    itinerary: [
+      "Day 1: Beaches + Water Sports",
+      "Day 2: Old Goa Churches + Fontainhas",
+      "Day 3: Chapora Fort + Night Market"
+    ]
+  },
+  {
+    name: "Tokyo, Japan",
+    places: ["Shibuya Crossing", "Senso-ji Temple", "Tokyo Skytree", "Akihabara"],
+    food: ["Sushi", "Ramen", "Takoyaki"],
+    itinerary: [
+      "Day 1: Senso-ji + Skytree",
+      "Day 2: Shibuya + Harajuku",
+      "Day 3: Akihabara + Imperial Palace"
+    ]
+  },
+  {
+    name: "Dubai, UAE",
+    places: ["Burj Khalifa", "Dubai Mall", "Palm Jumeirah", "Desert Safari"],
+    food: ["Shawarma", "Machboos", "Luqaimat"],
+    itinerary: [
+      "Day 1: Dubai Mall + Burj Khalifa",
+      "Day 2: Palm + Marina + JBR",
+      "Day 3: Desert Safari + Souks"
+    ]
+  }
+];
